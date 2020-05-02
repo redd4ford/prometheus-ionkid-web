8aa95d05dc41305d14ec0f7418c2877da6fc97ff
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-05-02 22:50:10 +0300

    Moved the principalExtractor logic to another class, and configured OAuth 2 login

--- src/main/java/com/prometheus/ionkid/config/WebSecurityConfig.java
+++ src/main/java/com/prometheus/ionkid/config/WebSecurityConfig.java
@@ -1,10 +1,7 @@
 package com.prometheus.ionkid.config;
 
-import com.prometheus.ionkid.dataaccess.UserRepository;
-import com.prometheus.ionkid.rest.model.Role;
-import com.prometheus.ionkid.rest.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,12 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -40,35 +36,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .csrf(c -> c
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
         )
-        .oauth2Login();
+        .oauth2Login( o -> o
+            .authorizationEndpoint()
+            .baseUri("/oauth2/authorization")
+            .authorizationRequestRepository(authorizationRequestRepository())
+        );
     http
         .httpBasic().disable();
   }
 
   @Bean
-  public PrincipalExtractor principalExtractor(@Autowired UserRepository userRepository) {
-    return map -> {
-      String id = (String) map.get("id");
-      User user = userRepository.findByGoogleId(id);
-      if (user == null) {
-        user = new User();
-        user.setGoogleId(id);
-        user.setFirstName((String) map.get("given_name"));
-        user.setLastName((String) map.get("family_name"));
-        user.setEmail((String) map.get("email"));
-        user.setGender((String) map.get("gender"));
-        user.setAvatarUrl((String) map.get("picture"));
-        //find a better way to set roles so that we don't have to create a new role for each user.
-        //maybe go for ENUM?
-        Set<Role> roles = new HashSet<>();
-        roles.add(new Role("ADMIN"));
-        user.setRoles(roles);
-      }
-      user.setLastVisit(LocalDateTime.now());
-      user.setActive(true);
-      userRepository.save(user);
-      return user;
-    };
+  public PrincipalExtractor googlePrincipalExtractor() {
+    return new GooglePrincipalExtractor();
+  }
+
+  @Bean
+  public AuthorizationRequestRepository<OAuth2AuthorizationRequest>
+         authorizationRequestRepository() {
+    return new HttpSessionOAuth2AuthorizationRequestRepository();
   }
 
 }
