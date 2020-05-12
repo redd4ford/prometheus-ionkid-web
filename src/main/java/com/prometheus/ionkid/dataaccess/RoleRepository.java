70b85fee8cdab947bfff00064c72806784d8ae38
Strashisvkyi <55955717+Strashisvkyi@users.noreply.github.com>
2020-05-12 17:38:10 +0300

    deleted role repository because we made role an enum

--- src/main/java/com/prometheus/ionkid/dataaccess/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prometheus.ionkid.dataaccess;
-
-import com.prometheus.ionkid.rest.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
