3a804ffcd21ff71b20266d5f0303e2bee0316e81
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-05-14 01:01:44 +0300

    Cleaned the code

--- src/main/java/com/prometheus/ionkid/dataaccess/TaskRepository.java
+++ src/main/java/com/prometheus/ionkid/dataaccess/TaskRepository.java
@@ -1,11 +1,8 @@
 package com.prometheus.ionkid.dataaccess;
 
-import com.prometheus.ionkid.rest.model.Program;
 import com.prometheus.ionkid.rest.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
