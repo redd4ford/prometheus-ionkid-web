01bd9f3a5f6fa6cf09ecc2079400961a77376ee0
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-05-14 01:02:23 +0300

    Cleaned the code

--- src/main/java/com/prometheus/ionkid/dataaccess/DoctorRepository.java
+++ src/main/java/com/prometheus/ionkid/dataaccess/DoctorRepository.java
@@ -4,8 +4,6 @@ import com.prometheus.ionkid.rest.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 
