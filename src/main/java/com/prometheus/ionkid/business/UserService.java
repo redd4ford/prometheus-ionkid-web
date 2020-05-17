e322bd0805ec855b3f469d8efc871beeb1a4430f
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-05-18 02:44:03 +0300

    Updated createOAuth2User and createNotOAuth2User methods, added isOAuth2User and updateLastVisit methods

--- src/main/java/com/prometheus/ionkid/business/UserService.java
+++ src/main/java/com/prometheus/ionkid/business/UserService.java
@@ -1,6 +1,5 @@
 package com.prometheus.ionkid.business;
 
-import com.prometheus.ionkid.dataaccess.DoctorRepository;
 import com.prometheus.ionkid.dataaccess.UserRepository;
 import com.prometheus.ionkid.rest.model.Doctor;
 import com.prometheus.ionkid.rest.model.Role;
@@ -8,6 +7,7 @@ import com.prometheus.ionkid.rest.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +19,13 @@ import java.util.List;
 public class UserService implements UserDetailsService {
   @Autowired
   private UserRepository userRepository;
-  @Autowired
-  private DoctorRepository doctorRepository;
 
   @Override
-  public User loadUserByUsername(String googleId) {
+  public User loadUserByUsername(String email) {
+    return userRepository.findByEmail(email);
+  }
+
+  public User loadUserByGoogleId(String googleId) {
     return userRepository.findByGoogleId(googleId);
   }
 
@@ -36,25 +38,43 @@ public class UserService implements UserDetailsService {
   }
 
   public void createOAuth2User(@AuthenticationPrincipal OAuth2User principal) {
-    String googleId = principal.getAttribute("sub");
-    User user = loadUserByUsername(googleId);
+    User user = loadUserByUsername(principal.getAttribute("email"));
     if (user == null) {
-      user = new Doctor();
-      user.setGoogleId(googleId);
-      user.setFirstName(principal.getAttribute("given_name"));
-      user.setLastName(principal.getAttribute("family_name"));
-      user.setEmail(principal.getAttribute("email"));
-      user.setGender(principal.getAttribute("gender"));
-      user.setAvatarUrl(principal.getAttribute("picture"));
-      user.setRoles(Collections.singleton(Role.DOCTOR));
+      String googleId = principal.getAttribute("sub");
+      user = loadUserByGoogleId(googleId);
+      if (user == null) {
+        user = new Doctor();
+        user.setGoogleId(googleId);
+        user.setFirstName(principal.getAttribute("given_name"));
+        user.setLastName(principal.getAttribute("family_name"));
+        user.setEmail(principal.getAttribute("email"));
+        user.setGender(principal.getAttribute("gender"));
+        user.setAvatarUrl(principal.getAttribute("picture"));
+        user.setRoles(Collections.singleton(Role.DOCTOR));
+      }
     }
-    user.setLastVisit(LocalDateTime.now());
+      updateLastVisit(user);
+      user.setActive(true);
+      userRepository.save(user);
+    }
+
+  public void createNotOAuth2User(User user) {
     user.setActive(true);
+    user.setRoles(Collections.singleton(Role.DOCTOR));
     userRepository.save(user);
   }
 
-  public void createNotOAuth2User(User user) {
-    userRepository.save(user);
+  public boolean isOAuth2User(User user) {
+    if (user.getGoogleId() != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void updateLastVisit(User user) {
+    user.setLastVisit(LocalDateTime.now());
+    update(user.getId(), user);
   }
 
   public User update(Integer id, User updatedUser) {
@@ -69,10 +89,6 @@ public class UserService implements UserDetailsService {
     if (userRepository.findById(id).isPresent()) {
       userRepository.deleteById(id);
     }
-  }
-
-  public User loadByEmail(String email) {
-    return userRepository.findByEmail(email);
   }
 
 }
