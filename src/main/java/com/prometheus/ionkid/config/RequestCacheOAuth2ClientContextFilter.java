feb7914195d17ebe9d9b67dedfc3d12c529677db
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-05-17 22:15:37 +0300

    Added redirect and authentication request handler

---
+++ src/main/java/com/prometheus/ionkid/config/RequestCacheOAuth2ClientContextFilter.java
@@ -0,0 +1,38 @@
+package com.prometheus.ionkid.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.UserRedirectRequiredException;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+
+public class RequestCacheOAuth2ClientContextFilter extends OAuth2ClientContextFilter {
+  private final RequestCache cache = new HttpSessionRequestCache();
+
+  @Override
+  protected void redirectUser(UserRedirectRequiredException e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    HttpServletRequestWrapper wrapper = new HttpServletRequestWrapper(request) {
+      @Override
+      public String getRequestURI() {
+        return URI.create(getReferer()).getPath();
+      }
+
+      @Override
+      public StringBuffer getRequestURL() {
+        return new StringBuffer(getReferer());
+      }
+
+      private String getReferer() {
+        return super.getHeader(HttpHeaders.REFERER);
+      }
+    };
+    cache.saveRequest(wrapper, response);
+    super.redirectUser(e, request, response);
+  }
+}
