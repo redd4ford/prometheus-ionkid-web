7f620577528fd5f504efd77b428ecb80e77a1530
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-05-22 00:13:34 +0300

    Added comments field in accordance with new database schema

--- src/main/java/com/prometheus/ionkid/rest/model/Task.java
+++ src/main/java/com/prometheus/ionkid/rest/model/Task.java
@@ -3,18 +3,20 @@ package com.prometheus.ionkid.rest.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Task {
+  @OneToMany(mappedBy = "task", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JsonIgnoreProperties("task")
+  public Set<Comment> comments;
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
-
   @Column
   private Boolean status;
   @Column
   private String description;
-
   @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
   @JoinColumn(name = "program_id")
   @JsonIgnoreProperties("tasks")
@@ -58,6 +60,14 @@ public class Task {
 
   public void setProgram(Program program) {
     this.program = program;
+  }
+
+  public Set<Comment> getComments() {
+    return comments;
+  }
+
+  public void setComments(Set<Comment> comments) {
+    this.comments = comments;
   }
 }
 
