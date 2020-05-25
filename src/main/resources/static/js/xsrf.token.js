9b7bf673899dbf878c1f2c4ce00ecbad6a2bf76a
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-05-25 19:54:12 +0300

    Added js file for XSRF token

---
+++ src/main/resources/static/js/xsrf.token.js
@@ -0,0 +1,9 @@
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (settings.type === 'POST' || settings.type === 'PUT' || settings.type === 'DELETE') {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                xhr.setRequestHeader("X-XSRF-TOKEN", Cookies.get('XSRF-TOKEN'));
+            }
+        }
+    }
+});
